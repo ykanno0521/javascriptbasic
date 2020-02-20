4ff748e086e208c0e2a0b448faae03518dea69bd
ykanno0521 <kanno0521@gmail.com>
2020-02-20 16:28:45 +0900

    js trial

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict';//エラーチェックのため
+
 {
-  const x = 100;
-  console.log(x);
+  document.querySelector('button').addEventListener('click', () => {
+    const li = document.createElement('li');
+    const color = document.querySelector('select');
+    li.textContent = `${color.value} - ${color.selectedIndex}`;
+    document.querySelector('ul').appendChild(li);
+  });
 }
