6b00eff72f0b017fb6afeda8a086f9ba5936a25c
ykanno0521 <kanno0521@gmail.com>
2020-02-21 09:54:52 +0900

    checkbox

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 {
   document.querySelector('button').addEventListener('click', () => {
     const colors = document.querySelectorAll('input');
-    let selectedColor;
+    const selectedColors = [];
 
     colors.forEach(color => {
       if (color.checked === true) {
-        selectedColor = color.value;
+        selectedColors.push(color.value);
       }
     });
     const li = document.createElement('li');
-    li.textContent = selectedColor;
+    li.textContent = selectedColors.join(',');
     document.querySelector('ul').appendChild(li);
   });
 }
